9882b8696d8c088aa799d67d220f9c40caa7ff77
Paramveer <126447825+Prabh45690@users.noreply.github.com>
2025-08-28 13:44:09 +0000

    change done

---
+++ 01_Basics/02datatypes.js
@@ -0,0 +1,25 @@
+"use sctrict"; //treat all js code as new version
+
+//alert( 3 + 3) // we are using node js
+
+console.log(3+3) // code readability should be high
+
+console.log("Prabh")
+
+let name ="Prabh"
+let age =20
+let isLoggedIn = false
+
+// number  => 2 to power 53
+// bigint 
+// string => ""
+// boolean => True/False
+//null 
+// undefined => 
+// symbol  => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+
+console.log(typeof null); // object 
